1d51bd95646654a5a5f2528e5d3b450ff2be2cf7
Kareemdub <kareem.werlander@metropolia.fi>
2025-10-20 11:06:08 +0300

    mod09 esimerkki

--- mod09/mod09_esim.py
+++ mod09/mod09_esim.py
@@ -1,23 +1,35 @@
 
 class Koira:
+    count = 0
     #konstruktori eli rakentaja
-    def __init__(self, name): #
-        print(f"uusi koiraolio {name}) luotu")
+    def __init__(self, name, weight): #
+        print(f"uusi koiraolio ({name}) luotu")
         self.name = name
+        self.weight = weight
+        Koira.count = Koira.count + 1
     #funktio, mutta luokassa joten metodi eli luokan toiminto
-    def hauku(self, toWhom):
-        print(f"{self.name} Hauhau {toWhom}")
+    def hauku(self, toWhom, times = 1):
+        print(f"{self.name} Haukkuu {toWhom}")
+        for i in range(times):
+            if self.weight < 10:
+               print("Wufwuf")
+            else:
+                print("Hauhau")
 
+koira1 = Koira("Räkky", 2)
+koira2 = Koira("Täkky", 22)
+koira3 = koira1
+koira4 = Koira(input("Anna koiralle nimi:"), 9)
 
-koira1 = Koira("Räkky")
-koira2 = Koira("Täkky")
-
-koira1.hauku("Matille") #matille parametri
+koira1.hauku("Matille", 2) #matille parametri
 koira2.hauku("Pekalle")
+koira4.hauku("Jarille", 1)
 
-
+koira1.name = "Wuffe"
+koira1.hauku("Matille")
 #koira1.name = "Räkky"
 #koira2.name = "Täkky"
+print(f"Koiraolioita on luotu {Koira.count}")
 """
 print(koira1) #printtaa koira olion ja muistipaikan
 print(koira1.name)
